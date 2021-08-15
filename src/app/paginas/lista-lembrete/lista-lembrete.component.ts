fe1a2514d64b6ccd2eb92293e4064a54bcc8f875
wander rodrigues <wanderrodrigues_@outlook.com>
2021-08-15 17:59:24 -0300

    :book: lista-lembrete

--- src/app/paginas/lista-lembrete/lista-lembrete.component.ts
+++ src/app/paginas/lista-lembrete/lista-lembrete.component.ts
@@ -3,4 +3,29 @@ import { Lembrete } from 'src/app/interfaces/lembrete';
 import { LembreteService } from 'src/app/services/lembrete.service';
 import { ErrorMsgComponent } from 'src/app/compatilhado/error-msg/error-msg.component';
 
+@Component({
+  selector: 'app-lista-lembrete',
+  templateUrl: './lista-lembrete.component.html',
+  styleUrls: ['./lista-lembrete.component.css']
+})
+export class ListaLembreteComponent implements OnInit {
 
+  public lembretes: Lembrete[];
+  @ViewChild(ErrorMsgComponent) errorMsgComponent: ErrorMsgComponent;
+
+  constructor(private lembreteService: LembreteService) { }
+
+  ngOnInit() {
+  }
+
+  getListaLembretes(){
+    this.lembreteService.getListaLembretes()
+    .subscribe((lembretes: Lembrete[])=> {
+      this.lembretes = lembretes;
+    }, () => { this.errorMsgComponent.setError('Falha ao buscar lembretes.'); });
+  }
+
+  existemLembretes(): boolean {
+    return this.lembretes && this.lembretes.length > 0;
+  }
+}
